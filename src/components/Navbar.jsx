c759254b1aaa59769b49c848c7085b7a9677c979
DouglasAshcroft <douglasashcroftjackson@gmail.com>
2025-08-05 12:44:35 -0400

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <span>
+      <nav className="navbar">
+        <h2>Unchained</h2>
+      </nav>
+    </span>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="container">
+      <h1>🎟 Welcome to Unchained</h1>
+      <h3>Buy tickets, Earn exclusive rewards, Own the moment!</h3>
+    </div>
+  );
+}
+export default Home;
